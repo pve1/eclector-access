851d38125de17adf8bf42071276331a0ade7b075
pve1 <pve1@foo.invalid>
2021-09-07 10:22:01 +0300

    defparameter -> defvar

--- eclector-access.lisp
+++ eclector-access.lisp
@@ -5,7 +5,7 @@
 
 (in-package #:eclector-access)
 
-(defparameter *standard-characters*
+(defvar *standard-characters*
   '(#\Newline #\Space
     #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9
     #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z
