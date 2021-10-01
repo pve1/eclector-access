a38433364c95deda57d50f9c8c2a074c2f897573
pve1 <pve1@foo.invalid>
2021-10-02 00:50:20 +0300

    Fixed mistake.

--- examples/extended-package-prefix-syntax-test.lisp
+++ examples/extended-package-prefix-syntax-test.lisp
@@ -4,12 +4,17 @@
 
 (in-package #:extended-package-prefix-syntax-test)
 
+;;; This file demonstrates the extended package prefix syntax as
+;;; implemented by SBCL.
+
 (eclector-access:enable
  (make-instance 'extended-package-prefix-syntax:reader))
 
 ;; Would fail.
-(let ((input "CL::(DEFUN ID (X) X))"))
-  (princ (nth-value 1 (ignore-errors (read-from-string input)))))
+(let ((input "CL::(DEFUN ID (X) X))")
+      (eclector.reader:*client*
+        (make-instance 'extended-package-prefix-syntax:reader)))
+  (princ (nth-value 1 (ignore-errors (eclector.reader:read-from-string input)))))
 
 ; => Lock on package COMMON-LISP violated when interning ID while in package
 ;    EXTENDED-PACKAGE-PREFIX-SYNTAX-TEST.
