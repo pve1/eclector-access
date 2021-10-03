52cca842adcb1cac1b82116ab57409d25430ce43
pve1 <pve1@foo.invalid>
2021-10-03 10:34:53 +0300

    Updated when-packages.

--- examples/when-packages-test.lisp
+++ examples/when-packages-test.lisp
@@ -1,13 +1,17 @@
 (defpackage #:when-packages-test
   (:use #:cl)
   (:import-from #:when-packages
-                #:when-packages))
+                #:when-packages
+                #:when-packages-dynamic))
 
 (in-package #:when-packages-test)
 
 ;;; This reader understands (when-packages PACKAGES &body BODY) forms,
 ;;; whose BODY may contain symbols that reference packages that do not
-;;; exist. The body is evaluated only if PACKAGES exist at runtime.
+;;; exist. The body of a ~WHEN-PACKAGES~ form will be evaluated if
+;;; PACKAGES exist when the form is read. The body of a
+;;; ~WHEN-PACKAGES-DYNAMIC~ form will be evaluated if PACKAGES exist
+;;; at runtime.
 
 (eclector-access:enable (make-instance 'when-packages::reader))
 
@@ -31,6 +35,19 @@
 
 (print (read-from-string
         "(when-packages (:foo)
+            (foo:a foo:b foo:c))"))
+
+; => NIL
+
+(print (read-from-string
+        "(when-packages (:when-packages-test.foo)
+            (foo:a foo:b foo:c))"))
+
+; => (PROGN (FOO::A FOO::B FOO::C))
+
+;; The dynamic variant uses eval.
+(print (read-from-string
+        "(when-packages-dynamic (:foo)
             (foo:a foo:b foo:c))"))
 
 ; => (WHEN (AND (FIND-PACKAGE ':FOO))
--- examples/when-packages.lisp
+++ examples/when-packages.lisp
@@ -1,7 +1,8 @@
 (defpackage #:when-packages
   (:use #:cl)
   (:export #:reader
-           #:when-packages))
+           #:when-packages
+           #:when-packages-dynamic))
 
 (in-package #:when-packages)
 
@@ -32,7 +33,11 @@
               nil)))
     (call-next-method)))
 
-;; Dummy macro
+;;; Dummy macros
+
+(defmacro when-packages-dynamic (packages &body body)
+  (declare (ignore packages body)))
+
 (defmacro when-packages (packages &body body)
   (declare (ignore packages body)))
 
@@ -46,12 +51,30 @@
            (eval (read-from-string ,(prin1-to-string `(progn ,@body))))))
       read-result))
 
+(defmethod eclector-access:translate-read-result ((client reader) (read-result cons))
+  (case (car read-result)
+    (when-packages-dynamic
+        (destructuring-bind (when-packages-dynamic packages &rest body)
+            read-result
+          (declare (ignore when-packages-dynamic))
+          `(when (and ,@(loop :for p :in packages
+                              :collect `(find-package ',p)))
+             (eval (read-from-string ,(prin1-to-string `(progn ,@body)))))))
+    (when-packages
+     (destructuring-bind (when-packages packages &rest body)
+         read-result
+       (declare (ignore when-packages))
+       (when (loop :for p :in packages :always (find-package p))
+         `(progn ,@body))))
+    (t read-result)))
+
 (defmethod eclector.reader:interpret-symbol ((client reader)
                                              input-stream
                                              package-indicator
                                              symbol-name
                                              internp)
-  (cond ((and (string= symbol-name "WHEN-PACKAGES")
+  (cond ((and (or (string= symbol-name "WHEN-PACKAGES")
+                  (string= symbol-name "WHEN-PACKAGES-DYNAMIC"))
               (eq :current package-indicator))
          (setf *allow-unknown-symbols-p* t)
          (call-next-method))
